032ce1a60c25d0923e5bebf312e5723e547cddcc
Anastasiia Kalyniuk <Kalyniuk.an@gmail.com>
2026-06-29 23:19:57 +0300

    int searchBar grad+-

---
+++ src/components/App/App.tsx
@@ -0,0 +1,9 @@
+import css from "./App.module.css";
+
+import SearchBar from "../SearchBar/SearchBar";
+
+export default function App() {
+  return (
+    <SearchBar></SearchBar>
+  )
+}
---
+++ src/components/MovoeGrid/MovieGrid.tsx
@@ -0,0 +1,27 @@
+import css from "./MovieGrid.module.css";
+import { Movie } from "../../types/movie";
+
+interface MovieGridProps {
+  movies: Movie[];
+}
+export default function MovieGrad({movies}: MovieGridProps) {
+  return (
+    <ul className={css.grid}>
+      /* Набір елементів списку з фільмами */
+      {movies.map((movie) =>
+        <li key={movie.id}>
+          <div className={css.card}>
+            <img
+              className={css.image}
+              src="https://image.tmdb.org/t/p/w500/poster-path"
+              alt={movie.title}
+              loading="lazy"
+            />
+            <h2 className={css.title}>{movie.title}</h2>
+          </div>
+        </li>
+      )
+      }
+</ul>
+  )
+}
