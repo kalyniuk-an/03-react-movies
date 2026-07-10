08f78096ca52cafcdb6a08332354faf335d08916
Anastasiia Kalyniuk <Kalyniuk.an@gmail.com>
2026-07-10 17:33:09 +0300

    fetch

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -2,17 +2,29 @@ import css from "./App.module.css";
 import { useState } from "react";
 
 import SearchBar from "../SearchBar/SearchBar";
+import { fetchMovie } from "../../services/movieService";
+import MovieGrid from "../MovoeGrid/MovieGrid";
 import Loader from "../Loader/Loader";
 import ErrorMessage from "../ErrorMessage/ErrorMessage";
+import type { Movie } from "../../types/movie";
 
 export default function App() {
+  const [movies, setMovies] = useState<Movie[]>([]);
   const [isLoading, setIsLoading] = useState<boolean>(false);
   const [isError, setIsError] = useState<boolean>(false);
 
-  const handleSubmit = async () => { 
+  const handleSubmit = async (query: string) => { 
     try {
       setIsLoading(true);
       setIsError(false);
+      setMovies([]);
+      const data = await fetchMovie(query);
+
+      if (data.length === 0) {
+        console.log("0");
+        return;
+      }
+      setMovies(data);
     } catch {
       setIsError(true);
     } finally {
@@ -23,6 +35,7 @@ export default function App() {
     <div className={css.app}>
       {/* <SearchBar onSearch={() => { }}></SearchBar> */}
       <SearchBar onSubmit={handleSubmit}></SearchBar>
+      {movies.length>0 && <MovieGrid onSelect={()=>{}} movies={movies}/>}
       {isLoading && <Loader />}
       {isError && <ErrorMessage />}
     </div>
