98100a13326b40f5cea386b305ec04607d85f17a
Anastasiia Kalyniuk <Kalyniuk.an@gmail.com>
2026-07-10 15:22:13 +0300

    upd

--- src/components/Loader/Loader.tsx
+++ src/components/Loader/Loader.tsx
@@ -1,4 +1,4 @@
-import css from "./MovieGrid.module.css";
+import css from "./Loader.module.css";
 
 export default function Loader() {
   return ( <p className={css.text}>Loading movies, please wait...</p> );
